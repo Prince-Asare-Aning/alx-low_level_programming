1d871fbdc036b62d52671b02ea78932f338dcf6f
Prince-Asare-Aning <111002508+Prince-Asare-Aning@users.noreply.github.com>
2022-10-25 12:40:41 +0000

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,32 +1,21 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_nodeint_end - A function that adds a new node to the end of list
- * @head: A pointer to list structure
+ * add_nodeint - A function that adds a new nod at the begining of a list
+ * @head: A pointer to list_t structure
  * @n: An integer data to put into the new node
- * Return: The address of the new element, or NULL on failure
+ * Return: the address of the new element, or NULL on failure
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node, *temp;
+	listint_t *new_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new_node = *head;
+	*head = malloc(sizeof(listint_t));
+	if (!*head)
 		return (NULL);
-
-	new_node->n = n;
-	if (*head == NULL)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else
-	{
-		new_node->next = NULL;
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new_node;
-	}
-	return (new_node);
+	(*head)->n = n;
+	(*head)->next = new_node;
+	return (*head);
 }
